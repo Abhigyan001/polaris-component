47c4f5a1c263ec2d52c18746249809662fb53d2e
Abhigyan001 <abhigyanmahanta001@gmail.com>
2025-05-23 14:57:12 -0400

    Add Customer model and controller

---
+++ app/components/customer_list_component.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+class CustomerListComponent < ViewComponent::Base
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,15 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Customer.destroy_all
+
+customers = [
+  { name: "Mae Jemison", location: "Decatur, USA" },
+  { name: "Ellen Ochoa", location: "Los Angeles, USA" },
+  { name: "Sally Ride", location: "San Diego, USA" },
+  { name: "Kalpana Chawla", location: "Houston, USA" },
+  { name: "Valentina Tereshkova", location: "Moscow, Russia" }
+]
+
+customers.each do |customer_data|
+  Customer.create!(customer_data)
+end
+
+puts "Created #{Customer.count} customers"
